f5594b8912e793513015645ba7f9ef8ef1937caf
Tejaaswini Narendra <tejnaren07@gmail.com>
2023-02-03 10:20:41 +0530

    init project| add playground

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 2020,
+        "sourceType": "module"
+    },
+    "rules": {
+       "indent": ["error", 2],
+       "semi": [2, "always"]
+    }
+};
---
+++ README.md
@@ -0,0 +1,8 @@
+# Rapidus
+
+> Requires GrapesJS v0.15.9 or higher
+
+## Steps to run
+
+#### npm install
+#### npm start
