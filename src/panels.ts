d4bbdad6738c1b6bb62912b0b51e21728d20d6fc
Tejaaswini Narendra <tejnaren07@gmail.com>
2023-02-05 14:07:07 +0530

    add save functionality

--- src/panels.ts
+++ src/panels.ts
@@ -6,6 +6,7 @@ import {
   cmdDeviceTablet,
   cmdDeviceMobile,
   cmdImportMjml,
+  cmdSaveMjml,
 } from './commands';
 
 export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
@@ -14,6 +15,43 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
 
   const getI18nLabel = (label: string) => editor.I18n.t(`grapesjs-mjml.panels.buttons.${label}`);
 
+
+  // Add save button
+  Panels.addButton('options', {
+    id: 'save-component',
+      className: 'btn-save-component',
+      label: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+      </svg>`,
+      context: 'save-component',
+      command(editor) {
+        editor.Modal.setTitle('Add custom component and save')
+          .setContent(`<textarea style="width:100%; height: 300px; background-color: #322931; color:#d5d3d5;"></textarea>
+          <button style="background-color: #52ad87; height: 40px; width: 100px; font-size: 16px; cursor: pointer;"><a href="/Users/tejaaswini.narendran/Desktop/rapidus/rapidus/demo/src/components/custom-components" download="cc.mjml">Save</a></button>`)
+          .open();
+      },})
+
+  // Add email
+  Panels.addButton('options', {
+    id: 'save-email',
+      className: 'btn-save-email',
+      label: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
+      <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z"/>
+    </svg>`,
+      context: 'save-email',
+      command(editor) {
+        editor.Modal.setTitle('Add custom component and save')
+          .setContent(`<form>
+          <div class="form-group">
+            <label for="exampleInputEmail1">From</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <label for="exampleInputEmail1">To</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <label for="exampleInputEmail1">Body</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <button style="background-color: #52ad87; height: 40px; width: 100px; font-size: 16px; cursor: pointer;">Send</button>`)
+          .open();
+      },})
 
   // Add Import button
   Panels.addButton('options', {
