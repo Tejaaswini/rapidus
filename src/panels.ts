4641c22f2e8214bb3585fe92222a0bfd5d6dac32
Tejaaswini Narendra <tejnaren07@gmail.com>
2023-02-07 14:40:55 +0530

    code cleanup

--- src/panels.ts
+++ src/panels.ts
@@ -15,23 +15,6 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
 
   const getI18nLabel = (label: string) => editor.I18n.t(`grapesjs-mjml.panels.buttons.${label}`);
 
-
-  // Add send email button
-  Panels.addButton('options', {
-    id: 'save-component',
-      className: 'btn-save-component',
-      label: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-      </svg>`,
-      context: 'save-component',
-      command(editor) {
-        editor.Modal.setTitle('Send Email')
-          .setContent(`<form action="mailto:tejnaren07@gmail.com" method="post" enctype="text/plain"><textarea style="width:100%; height: 300px; background-color: #322931; color:#d5d3d5;">${cmdImportMjml}</textarea>
-          <button style="background-color: #52ad87; height: 40px; width: 100px; font-size: 16px; cursor: pointer;">Send</button></form>`)
-          .open();
-      },})
-
-
   // Add Import button
   Panels.addButton('options', {
     id: 'import-figma',
